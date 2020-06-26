7a48534242805556447b093a1c144a094360a401
Roman Croitor <croitorr@gmail.com>
2020-06-26 12:18:11 +0300

    Pie Chart Debug

--- ChartsApp/PieViewController.swift
+++ ChartsApp/PieViewController.swift
@@ -20,6 +20,17 @@ class PieViewController: UIViewController, ChartViewDelegate {
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
+        pieChart.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.width)
+        pieChart.center = view.center
+        view.addSubview(pieChart)
         
+        var entries = [ChartDataEntry]()
+        for number in 0..<5 {
+            entries.append(ChartDataEntry(x: Double(number), y: Double.random(in: 0.0...Double(number))))
+        }
+        let set = PieChartDataSet(entries: entries)
+        set.colors = ChartColorTemplates.colorful()
+        let pieChartData = PieChartData(dataSet: set)
+        pieChart.data = pieChartData
     }
 }
